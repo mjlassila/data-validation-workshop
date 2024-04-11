8dc8419b5a7c39ddf1d63435b7b53cf3a8436557
Matti Lassila <matti.lassila@gmail.com>
2024-04-11 11:37:14 +0300

    Added rules to te SBS2000 validation set, validated the data using rules and summarized the results. Finishes assignment 2.

--- code/SBS2000_rules.R
+++ code/SBS2000_rules.R
@@ -1,4 +1,5 @@
 # validation rules for the SBS2000 data set.
+# values are thousands of euros
 # By Vi the Validator, 2021.
 
 # range checks
@@ -13,3 +14,18 @@ turnover + other.rev == total.rev
 # People don't usually work for free in retail
 if (staff > 0) staff.costs >= 0
 
+# Paid VAT should be always positive
+
+vat > 0
+
+# If there are losses, they should be relative to the size of the org
+
+if (profit < 0) profit / staff > -2 
+
+# Is the average profit positive
+
+mean(profit) > 0
+
+# Does turnover - cost equal profit
+
+turnover - total.costs == profit
